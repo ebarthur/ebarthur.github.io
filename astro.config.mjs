5c4ee573a08654c3259ff2c1a79103e33f46d37f
Ebenezer Arthur <arthurebenezer@aol.com>
2025-07-21 20:16:15 +0000

    configure formatter

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,27 +8,32 @@ import rehypeSlug from "rehype-slug";
 import remarkGithubAlerts from "remark-github-alerts";
 import UnoCSS from "unocss/astro";
 
-
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ebarthur.github.io",
-    integrations: [UnoCSS({
-        injectReset: true,
-		}), mdx(), sitemap()],
+  site: "https://ebarthur.github.io",
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+    mdx(),
+    sitemap(),
+  ],
 
-    markdown: {
-        shikiConfig: {
-            themes: {
-                        light: "snazzy-light",
-                dark: "vitesse-dark",
-            },
-        },
-        remarkPlugins: [remarkGithubAlerts],
-        rehypePlugins: [
-            rehypeSlug,
-            rehypeKatext,
-						 [rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] }]
-        ],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "snazzy-light",
+        dark: "vitesse-dark",
+      },
     },
-		
+    remarkPlugins: [remarkGithubAlerts],
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeKatext,
+      [
+        rehypeExternalLinks,
+        { target: "_blank", rel: ["noopener", "noreferrer"] },
+      ],
+    ],
+  },
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,14 +2,14 @@ import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const writing = defineCollection({
-	loader: glob({ pattern: "**/*.md", base: "./src/content/writing" }),
-	schema: z.object({
-		createdAt: z.coerce.date(),
-		title: z.string(),
-		description: z.string(),
-		thumbnail: z.string().url().optional(),
-		tags: z.array(z.string()).optional()
-	}),
+  loader: glob({ pattern: "**/*.md", base: "./src/content/writing" }),
+  schema: z.object({
+    createdAt: z.coerce.date(),
+    title: z.string(),
+    description: z.string(),
+    thumbnail: z.string().url().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
 });
 
 export const collections = { writing };
